8711926d0fbd0cd8b9f8c15962f0a176ed8fe9b7
Aditya Kumar <2005aditya.k@gmail.com>
2024-08-26 19:14:20 +0530

    first code

---
+++ CPP/01.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+/*Write a code for taking in 5 numbers as input in an array and printing them.
+
+int main(){
+    int arr[5];
+    for(int i=0;i<5;i++){
+        int x;
+        cin >> x;
+        arr[i]=x;
+    }
+    
+    for (int i:arr){
+        cout <<" ";
+        cout << i;
+    }
+
+}
+
+*/
