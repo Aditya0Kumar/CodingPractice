a08e5e31d94a3fa024aa1e9ba42d238c8f07b447
Aditya Kumar <2005aditya.k@gmail.com>
2024-08-27 19:39:02 +0530

    Palendrome

---
+++ CPP/palendrome.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+bool ispalendrome(char arr[],int size){
+    int s=0;
+    int e = size-1;
+    while(s<e){
+        if(arr[s]!=arr[e]){
+            return false;
+        }
+        else{
+            s++;
+            e--;
+        }
+        return true;
+    }
+}
+
+int main(){
+    char arr[100]="noon";
+    if (ispalendrome(arr,4)){
+        cout<<"Palendrome";
+    }
+    else{
+        cout<<"Not Palendrome";
+    }
+}
