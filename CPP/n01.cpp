f9ae927aed3954586a9ad3bdc049a5c9bdca2afd
Aditya Kumar <2005aditya.k@gmail.com>
2024-12-31 14:59:36 +0530

    Conversion

---
+++ CPP/n01.cpp
@@ -0,0 +1,117 @@
+// #include <iostream>
+// using namespace std;
+
+// class Angle {
+// public:
+//     float degrees;
+//     Angle(float d) : degrees(d) {}
+//     operator float() {
+//         return degrees-8.9;
+//     }
+// };
+
+// int main() {
+//     Angle a(45.5);
+//     int result = a;
+//     cout << result;
+//     return 0;
+// }
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// class Distance {
+// public:
+//     int meters;
+//     Distance(int m) : meters(m) {}
+//     operator int() {
+//         return meters + 78;
+//     }
+// };
+
+// int main() {
+//     Distance d(500);
+//     int result = d;
+//     cout << result;
+//     return 0;
+// }
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// class Time {
+// public:
+//     int minutes;
+//     Time(int m) : minutes(m) {}
+//     operator string() {
+//         return to_string(minutes+78) + " mins";
+//     }
+// };
+
+// int main() {
+//     Time t(120);
+//     string result = t;
+//     cout << result;
+//     return 0;
+// }
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// class Time {
+// public:
+//     int minutes;
+//     Time(int m) : minutes(m) {}
+//     operator string() {
+//         return to_string(minutes+78) + " mins";
+//     }
+// };
+
+// int main() {
+//     Time t(120);
+//     string result = t;
+//     cout << result;
+//     return 0;
+// }
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// class Time {
+// public:
+//     int minutes;
+//     Time(int m) : minutes(m) {}
+//     operator string() {
+//         return to_string(minutes+78) + " mins";
+//     }
+// };
+
+// int main() {
+//     Time t(120);
+//     string result = t;
+//     cout << result;
+//     return 0;
+// }
+
+
+
+
+
