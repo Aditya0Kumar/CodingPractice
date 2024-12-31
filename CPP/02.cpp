9efb25f0c0d25ab3fb204ffd9b296dd52d6ef5e0
Aditya Kumar <2005aditya.k@gmail.com>
2024-12-31 14:58:06 +0530

    02.cpp added

---
+++ CPP/02.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    string s =" Constellations are made of star in the sky.";
+    string w ="star";
+    for (int i=0;i<s.size();i++){
+        if (w==s){
+            
+        }
+    }
+}
